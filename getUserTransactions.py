af4d888036af0add831b83607621d8a1e375844d
Mohammed Anees <anees@Mohammeds-MacBook-Air.local>
2023-09-09 14:28:50 +0530

    print transaction id of user to file

--- getUserTransactions.py
+++ getUserTransactions.py
@@ -20,4 +20,9 @@ user_info_json = user_info_response.json()
 
 user_txs = user_info_json.get("txs",[])
 
+with open("./user_transaction_ids" , "w+") as file:
+    for tx in user_txs:
+        tx_hash = tx.get("hash")
+        file.write(tx_hash+'\n')
 
+file.close()
