ca9a2ab4cc4f3da4a33bfc498351085e08fe5ad8
Radhika Swaminathan <radhu.shoba@gmail.com>
2023-09-12 12:47:29 +0530

    basic graph

--- draw_graph.py
+++ draw_graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-DG = nx.petersen_graph()
+DG = nx.DiGraph()
 
 def create_node(parent_tx,child_tx):
 
@@ -13,14 +13,3 @@ def create_node(parent_tx,child_tx):
     DG.add_edge(parent_tx,child_tx)
 
     nx.spring_layout(DG)
-
-
-    pos = nx.spring_layout(DG)
-    nx.draw_networkx_nodes(DG, pos, cmap=plt.get_cmap('jet'), 
-                         node_size = 500)
-    nx.draw_networkx_labels(DG, pos)
-    nx.draw_networkx_edges(DG, pos,  edge_color='r', arrows=True)
-    nx.draw_networkx_edges(DG, pos, arrows=False)
-    plt.show()
-
-create_node(1,2)
