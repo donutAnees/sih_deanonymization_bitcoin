ebc55655248e08ebf08b362ebc770c43364ca30f
Mohammed Anees <pvmohammedanees2003@gmail.com>
2023-12-08 12:06:19 +0530

    illegal wallet ds

--- backend/get_all_user_txs.py
+++ backend/get_all_user_txs.py
@@ -2,15 +2,20 @@ import requests
 
 def get_all_transaction(user_hash):
 
-    user_info_response = requests.get("https://api.blockcypher.com/v1/btc/main/txs/" + user_hash + "?token=cd380b7fda6a44909bff4645ec8b0448")
+    user_info_response = requests.get("https://api.blockcypher.com/v1/btc/main/addrs/" + user_hash + "?token=cd380b7fda6a44909bff4645ec8b0448")
 
     user_info_json = user_info_response.json()
 
     user_txs = user_info_json.get("txrefs",[])
 
-    with open("./transaction_folder/" + user_hash , "a+") as file:
+    with open("./wallets/" + user_hash , "a+") as file:
          for tx in user_txs:
             file.write(str(tx)+'\n')
     file.close()
 
-get_all_transaction("1FwYmGEjXhMtxpWDpUXwLx7ndLNfFQncKq")
+if __name__ == "__main__":
+    with open("./illegal wallets.txt") as file:
+        count = 0
+        for wallet_id in file:
+           get_all_transaction(wallet_id.strip())
+    file.close() 
