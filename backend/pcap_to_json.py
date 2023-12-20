bb3d9b149b5606fb07efde9ffbb8fe3ae5637088
Deepti <sai.krishna.deepti@gmail.com>
2023-12-20 09:34:47 +0530

    done-deepti

--- backend/pcap_to_json.py
+++ backend/pcap_to_json.py
@@ -5,8 +5,6 @@ print("enter input and output filenames:")
 input, output= input().split()
 
 output_filename=output+'.json'
-output_extracted_json= 'outputextracted.json'
-
 cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e tcp.srcport -e tcp.dstport -e frame.time -e frame.time_epoch -e tcp.payload -e bitcoin.command -E separator=, -E header=y > {}.json'
 #cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e data.data -E separator=, -E header=y > {}.json'
 final_cmd = cmd.format(input, output)
