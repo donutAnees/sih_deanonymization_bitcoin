fc6a89a9c912055703724560354c7b48bd1a7de9
Deepti <sai.krishna.deepti@gmail.com>
2023-12-20 09:27:39 +0530

    new pcapng to json stuff-deepti

--- backend/pcap_to_json.py
+++ backend/pcap_to_json.py
@@ -1,11 +1,13 @@
 import os
-import pandas as pd
+import json
 
 print("enter input and output filenames:")
 input, output= input().split()
 
 output_filename=output+'.json'
+output_extracted_json= 'outputextracted.json'
 
-cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e data.data -E separator=, -E header=y > {}.json'
+cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e tcp.payload -e bitcoin.command -E separator=, -E header=y > {}.json'
+#cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e data.data -E separator=, -E header=y > {}.json'
 final_cmd = cmd.format(input, output)
 os.system(final_cmd)
