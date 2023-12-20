6d46e52da55a1e5ce3f46f89bed778af807bf58e
Deepti <sai.krishna.deepti@gmail.com>
2023-12-20 09:32:22 +0530

    changes with tcp ports too -deepti

--- backend/pcap_to_json.py
+++ backend/pcap_to_json.py
@@ -7,7 +7,7 @@ input, output= input().split()
 output_filename=output+'.json'
 output_extracted_json= 'outputextracted.json'
 
-cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e tcp.payload -e bitcoin.command -E separator=, -E header=y > {}.json'
+cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e tcp.srcport -e tcp.dstport -e frame.time -e frame.time_epoch -e tcp.payload -e bitcoin.command -E separator=, -E header=y > {}.json'
 #cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e data.data -E separator=, -E header=y > {}.json'
 final_cmd = cmd.format(input, output)
 os.system(final_cmd)
