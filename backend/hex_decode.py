b9f95024f28657d1bd998d30cbea26352426229b
Mohammed Anees <anees@Mohammeds-MacBook-Air.local>
2023-12-28 20:56:03 +0530

    update

--- backend/hex_decode.py
+++
@@ -1,5 +0,0 @@
-from cryptotools.BTC import Transaction
-
-def hex_decode(hex):
-    tx = Transaction.from_hex(hex)
-    return tx.json()
--- backend/pcap_to_json.py
+++ backend/pcap_to_json.py
@@ -5,7 +5,7 @@ print("enter input and output filenames:")
 input, output= input().split()
 
 output_filename=output+'.json'
-cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e tcp.srcport -e tcp.dstport -e frame.time -e frame.time_epoch -e tcp.payload -e bitcoin.command -E separator=, -E header=y > {}.json'
+cmd ='tshark -r {}.pcapng -T json > {}.json'
 #cmd ='tshark -r {}.pcapng -T json -e ip.dst -e ip.src -e frame.time -e frame.time_epoch -e data.data -E separator=, -E header=y > {}.json'
 final_cmd = cmd.format(input, output)
 os.system(final_cmd)
