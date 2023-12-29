7a68cb098c7b77fc183830af9004009632e863e2
Mohammed Anees <anees@Mohammeds-MacBook-Air.local>
2023-12-29 22:08:22 +0530

    done

--- backend/parser.py
+++ backend/parser.py
@@ -1,13 +1,9 @@
-#get_address("473044022001bbb98ef9070d68ab34d2402becf21c6a728f138f550cba358f6f5631c6bf300220450b4ae16a45217cb8c2d43929b9c3968c065c30e1bc5caf6ebfb2e48aac079101210308d8aff8763b126a66bb4192cc5d4c63bb3d7c471cd8c20b270a609090e86a3a", "01")
-#get_address("eb32f14fb4458bbfa5184ba1dd53b64b08cbdb60710e7aec29ce04c029f50594", "01")
-
-#get_address("473044022100edf664ebfc12a3998d242fbdd8a1a44c13e4e7f6d7dbddde9ae5610bfd2c9c38021f74ec1e2eca7310baa17c60cbaaffa0503cee680f13fd9a10fd04f2910958c9012103486669962008e0713660b6d69117a65fcecd221d06c1e5077b4d9cd477c0cf98", "01")
-
 import base58
 from hashlib import sha256
 import hashlib
 
 def get_address(public_key , version):
+
     public_key = public_key.replace(":","")
     '''
     length_in_string = script[:2]
@@ -29,7 +25,6 @@ def get_address(public_key , version):
     #ripemd160_script = hashlib.new('ripemd160', hashlib.sha256(public_key.encode()).digest())
     r = r.hexdigest()
 
-    # version= '0'# ??????????????????????????????????????????????????????????????????????????????????????????????????
     extended_public_key= version+ r
 
 
@@ -42,8 +37,3 @@ def get_address(public_key , version):
     #encode258
     address = base58.b58encode(bytes.fromhex(addr_in_hex)).decode('utf-8')
     return address
-
-
-pub_key="76:a9:14:c5:ad:98:b9:12:6f:d6:f8:5a:4b:24:0b:a6:79:50:49:fe:de:2c:32:88:ac"
-addr=get_address(pub_key,"00")
-print(addr)
